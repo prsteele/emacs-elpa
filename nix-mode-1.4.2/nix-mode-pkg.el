6e15be940b44c57dc0d0889c77120f5557676ef9
Patrick Steele <steele.pat@gmail.com>
2020-07-14 19:14:27 -0400

    Add nix-mode

---
+++ nix-mode-1.4.2/nix-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "nix-mode" "1.4.2" "Major mode for editing .nix files"
+  '((emacs "25"))
+  :commit "e20afb51a48b5d014292d123487f53a678c7c3f7" :keywords
+  '("nix" "languages" "tools" "unix")
+  :maintainer
+  '("Matthew Bauer" . "mjbauer95@gmail.com")
+  :url "https://github.com/NixOS/nix-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
